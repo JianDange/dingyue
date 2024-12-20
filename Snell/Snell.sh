1f94efc13ada53262f15eec68c0cf9dcfa09b955
JianDange <130828417+JianDange@users.noreply.github.com>
2024-12-20 17:09:35 +0800

    更新 Snell.sh

--- Snell/Snell.sh
+++ Snell/Snell.sh
@@ -83,7 +83,7 @@ install_snell() {
         RANDOM_PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 31)
         cat > ${CONF_FILE} << EOF
 [snell-server]
-dns = 127.0.0.1
+dns = 1.1.1.1 8.8.8.8
 listen = ::0:${RANDOM_PORT}
 psk = ${RANDOM_PSK}
 ipv6 = true
